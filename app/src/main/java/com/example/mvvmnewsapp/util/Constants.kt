f2f4d0d98452122b736a006988efdc914e5ffca2
Duc-267 <mduc2672002@gmail.com>
2021-09-28 12:31:59 +0700

    navigation setup

---
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "e1bf947485ad43beb1bdbeb27d52aa6f"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
